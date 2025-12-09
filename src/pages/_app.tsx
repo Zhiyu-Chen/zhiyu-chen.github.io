e7c995bd4d7fbf1c1b2170a7c0a173f391d694bb
Zhiyu Chen <Zhiyu-Chen@users.noreply.github.com>
2025-12-09 10:28:20 -0800

    Feat: Integrate Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import RobotCursor from "@/components/RobotCursor";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-81JL9108VP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-81JL9108VP');
+        `}
+      </Script>
       <RobotCursor />
       <Component {...pageProps} />
     </>
